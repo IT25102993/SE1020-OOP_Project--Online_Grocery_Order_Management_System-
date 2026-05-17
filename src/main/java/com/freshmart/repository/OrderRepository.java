86c563f1a91a0cd212567f4b816e74a45d70f95f
IT25102191 <IT25102191@my.sliit.lk>
2026-05-17 15:57:42 +0530

    customer Dto

--- src/main/java/com/freshmart/repository/OrderRepository.java
+++ src/main/java/com/freshmart/repository/OrderRepository.java
@@ -1,7 +1,6 @@
 package com.freshmart.repository;
 
 import com.freshmart.entity.Order;
-import com.freshmart.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
