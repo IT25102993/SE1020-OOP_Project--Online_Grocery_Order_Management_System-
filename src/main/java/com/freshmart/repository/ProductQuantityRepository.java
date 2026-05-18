e013ddf635e4afd5b62805f878eb6b1f678d9f41
IT25102993 <it25102993@my.sliit.lk>
2026-05-18 19:50:32 +0530

    Delete ProductQuantityRepository.java

--- src/main/java/com/freshmart/repository/ProductQuantityRepository.java
+++
@@ -1,8 +0,0 @@
-package com.freshmart.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface ProductQuantityRepository extends JpaRepository<ProductQuantity, Long> {
-    List<ProductQuantity> findByProductIdOrderByCreatedAtDesc(String productId);
-}
