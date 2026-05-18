a46081bedf8ba439d1ee49e2b2bd1f4b03926e7b
IT25102993 <it25102993@my.sliit.lk>
2026-05-18 19:50:20 +0530

    Delete DriverRepository.java

--- src/main/java/com/freshmart/repository/DriverRepository.java
+++
@@ -1,7 +0,0 @@
-package com.freshmart.repository;
-
-import com.freshmart.entity.Driver;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DriverRepository extends JpaRepository<Driver, Long> {
-}
