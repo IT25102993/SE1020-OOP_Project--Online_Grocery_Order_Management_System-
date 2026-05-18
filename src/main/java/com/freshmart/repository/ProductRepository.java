8bf6597cea4f5432c1fea16693d290d05ad64e58
IT25102690 <it25102690@my.sliit.lk>
2026-05-18 14:01:54 +0530

    delete sample code

--- src/main/java/com/freshmart/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.freshmart.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-    Optional<Product> findByProductId(String productId);
-    boolean existsByProductId(String productId);
-}
